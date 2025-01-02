1d1e759d7092e68b061e93be97567536d00f2511
angelcruz07 <angelcruzsanchez126@gmail.com>
2025-01-01 19:31:11 -0600

    ci: progrees in script auto installation

--- install-arch-linux.sh
+++ install-arch-linux.sh
@@ -2,78 +2,152 @@
 
 set -e
 
-# Define colors for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # No Color
+# Define colors for output using tput for better compatibility
+PINK=$(tput setaf 204)
+PURPLE=$(tput setaf 141)
+GREEN=$(tput setaf 114)
+ORANGE=$(tput setaf 208)
+BLUE=$(tput setaf 75)
+YELLOW=$(tput setaf 221)
+RED=$(tput setaf 196)
+NC=$(tput sgr0) # No Color
 
 USER_HOME=$(eval echo ~${SUDO_USER})
 
-echo -e "${GREEN}Welcome to the AlexCode installation and configuration guide!${NC}"
+echo -e "${GREEN}Welcome to the AlexCode auto installation${NC}"
+
+sudo -v
+
+while true; do
+  sudo -n true
+  sleep 60
+  kill - "$$" || exit
+done 2>/dev/null &
+
+select_option() {
+  local prompt_message="$1"
+  shift
+  local options=("$@")
+  PS3="${ORANGE}$prompt_message${NC} "
+  select opt in "${options[@]}"; do
+    if [ -n "$opt" ]; then
+      echo "$opt"
+      break
+    else
+      echo -e "${RED}Invalid option. Please try again.${NC}"
+    fi
+  done
+}
 
 # Function to prompt user for input
 prompt_user() {
   local prompt_message="$1"
   local default_answer="$2"
-  read -p "$prompt_message [$default_answer] " user_input
+  read -p "$(echo -e ${BLUE}$prompt_message [$default_answer]${NC}) " user_input
   user_input="${user_input:-$default_answer}"
   echo "$user_input"
 }
 
+# Function to display a spinner
+spinner() {
+  local pid=$!
+  local delay=0.1
+  local spinstr='|/-\'
+  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+    local temp=${spinstr#?}
+    printf " [%c]  " "$spinstr"
+    spinstr=$temp${spinstr%"$temp"}
+    sleep $delay
+    printf "\b\b\b\b\b\b"
+  done
+  printf "    \b\b\b\b"
+}
+
+# Function to check and create directories if they do not exist
+ensure_directory_exists() {
+  local dir_path="$1"
+  local create_templates="$2"
+
+  if [ ! -d "$dir_path" ]; then
+    echo -e "${YELLOW}Directory $dir_path does not exist. Creating...${NC}"
+    mkdir -p "$dir_path"
+  else
+    echo -e "${GREEN}Directory $dir_path already exists.${NC}"
+  fi
+
+  # Check for the "templates" directory only if create_templates is true
+  if [ "$create_templates" == "true" ]; then
+    if [ ! -d "$dir_path/templates" ]; then
+      echo -e "${YELLOW}Templates directory does not exist. Creating...${NC}"
+      mkdir -p "$dir_path/templates"
+      echo -e "${GREEN}Templates directory created at $dir_path/templates${NC}"
+    else
+      echo -e "${GREEN}Templates directory already exists at $dir_path/templates${NC}"
+    fi
+  fi
+}
+
+# Function to run commands with optional suppression of output
+run_command() {
+  local command=$1
+  if [ "$show_details" = "Yes" ]; then
+    eval $command
+  else
+    eval $command &>/dev/null
+  fi
+}
+
 # Function to install basic dependencies for Arch Linux
 install_dependencies() {
-  if [ "$os_choice" = "linux" ]; then
-    # Update the package database
-    sudo pacman -Syu --noconfirm
+  run_command "sudo pacman -Syu --noconfirm"
+  run_command "sudo pacman -S --needed --noconfirm base-devel curl file git wget"
+  run_command "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
+  run_command ". $HOME/.cargo/env"
 
-    # Install the necessary packages
-    sudo pacman -S --noconfirm base-devel curl file git
+}
+
+install_homebrew_with_progress() {
+  local install_command="$1"
+
+  echo -e "${YELLOW}Installing Homebrew...${NC}"
+
+  if [ "$show_details" = "No" ]; then
+    # Run installation in the background and show progress
+    (eval "$install_command" &>/dev/null) &
+    spinner
+  else
+    # Run installation normally
+    eval "$install_command"
   fi
 }
 
 # Function to install Homebrew if not installed
-
 install_homebrew() {
   if ! command -v brew &>/dev/null; then
     echo -e "${YELLOW}Homebrew is not installed. Installing Homebrew...${NC}"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    if [ "$show_details" = "No" ]; then
+      # Show progress bar while installing Homebrew
+      install_homebrew_with_progress "/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
+      spinner
+    else
+      # Install Homebrew normally
+      run_command "/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
+    fi
 
     # Add Homebrew to PATH based on OS
     if [ "$os_choice" = "mac" ]; then
-      (
-        echo
-        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
-      ) >>$USER_HOME/.zshrc
-      (
-        echo
-        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
-      ) >>$USER_HOME/.bashrc
-      mkdir -p $USER_HOME/.config/fish
-      (
-        echo
-        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
-      ) >>$USER_HOME/.config/fish/config.fish
-      eval "$(/opt/homebrew/bin/brew shellenv)"
+      run_command "(echo 'eval \"\$(/opt/homebrew/bin/brew shellenv)\"' >> $USER_HOME/.zshrc)"
+      run_command "(echo 'eval \"\$(/opt/homebrew/bin/brew shellenv)\"' >> $USER_HOME/.bashrc)"
+      run_command "mkdir -p $USER_HOME/.config/fish"
+      run_command "(echo 'eval \"\$(/opt/homebrew/bin/brew shellenv)\"' >> $USER_HOME/.config/fish/config.fish)"
+      run_command "eval \"\$(/opt/homebrew/bin/brew shellenv)\""
     elif [ "$os_choice" = "linux" ]; then
-      if [ -d "/home/linuxbrew/.linuxbrew" ]; then
-        (
-          echo
-          echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
-        ) >>$USER_HOME/.zshrc
-        (
-          echo
-          echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
-        ) >>$USER_HOME/.bashrc
-        mkdir -p $USER_HOME/.config/fish
-        (
-          echo
-          echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
-        ) >>$USER_HOME/.config/fish/config.fish
-        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-      else
-        echo -e "${RED}Homebrew installation directory not found. Please check the installation.${NC}"
-      fi
+      run_command "(echo 'eval \"\$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)\"' >> ~/.zshrc)"
+      run_command "(echo 'eval \"\$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)\"' >> ~/.bashrc)"
+      run_command "mkdir -p ~/.config/fish"
+      run_command "(echo 'eval \"\$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)\"' >> ~/.config/fish/config.fish)"
+      run_command "eval \"\$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)\""
     fi
   else
     echo -e "${GREEN}Homebrew is already installed.${NC}"
@@ -87,7 +161,6 @@ update_or_replace() {
   local replace="$3"
 
   if grep -q "$search" "$file"; then
-    # Use awk to replace the line containing the search string
     awk -v search="$search" -v replace="$replace" '
     $0 ~ search {print replace; next}
     {print}
@@ -97,97 +170,52 @@ update_or_replace() {
   fi
 }
 
-# Function to set the default shell
-set_default_shell() {
-  local shell_path="$1"
+# Ask if the user wants to see detailed output
+show_details="Yes"
 
-  if ! grep -Fxq "$shell_path" /etc/shells; then
-    echo "$shell_path" | sudo tee -a /etc/shells
+os_choice=$(select_option "Are you sure this is on a clean Arch installation? " "yes" "no")
+
+if [ "$os_choice" = "yes" ]; then
+  # Install basic dependencies with progress bar
+  echo -e "${YELLOW}Installing basic dependencies...${NC}"
+  if [ "$show_details" = "No" ]; then
+    install_dependencies &
+    spinner
+  else
+    install_dependencies
   fi
+elif [ "$os_choice" = "no" ]; then
+  echo -e "${RED}It is not possible to continue without a clean Arch installation.${NC}"
+  exit 1
+else
+  echo -e "${RED}Invalid choice. Exiting.${NC}"
+  exit 1
 
-  sudo chsh -s "$shell_path" "$USER"
+fi
+
+# Function to clone repository with progress bar
+clone_repository_with_progress() {
+  local repo_url="$1"
+  local clone_dir="$2"
+  local progress_duration=$3
+
+  echo -e "${YELLOW}Cloning repository...${NC}"
+
+  if [ "$show_details" = "No" ]; then
+    # Run clone command in the background and show progress
+    (git clone "$repo_url" "$clone_dir" &>/dev/null) &
+    spinner "$progress_duration"
+  else
+    # Run clone command normally
+    git clone "$repo_url" "$clone_dir"
+  fi
 }
-
-# Ask for the operating system
-os_choice=$(prompt_user "Which operating system are you using? (Options: mac, linux)" "none")
-
-# Install basic dependencies
-install_dependencies
-
-PROJECT_PATHS=$(prompt_user "Enter the path for your projects" "/your/work/path/")
 
 # Step 1: Clone the Repository
 echo -e "${YELLOW}Step 1: Clone the Repository${NC}"
-if [ -d "dotfiles" ]; then
-  echo -e "${GREEN}Repository already cloned. Skipping...${NC}"
-else
-  git clone https://github.com/angelcruz07/dotfiles.git
+if [ -d "Gentleman.Dots" ]; then
+  echo -e "${GREEN}Repository already cloned. Overwriting...${NC}"
+  rm -rf "AlexCode Dots"
 fi
-cd dotfiles || exit
-
-# Install Homebrew if not installed
-install_homebrew
-
-# Terminal Emulators Installation
-echo -e "${YELLOW}Step 2: Choose and Install Terminal Emulator${NC}"
-if is_wsl; then
-  echo -e "${YELLOW}You are running WSL. Terminal emulators should be installed on Windows.${NC}"
-else
-  if [ "$os_choice" = "linux" ]; then
-    echo -e "${YELLOW}Note: Kitty is available for Linux, but we will adapt the script accordingly.${NC}"
-    term_choice=$(prompt_user "Which terminal emulator do you want to install? (Options: alacritty, wezterm, kitty)" "none")
-  else
-    term_choice=$(prompt_user "Which terminal emulator do you want to install? (Options: alacritty, wezterm, kitty)" "none")
-  fi
-  case "$term_choice" in
-  "alacritty")
-    if ! command -v alacritty &>/dev/null; then
-      if [ "$os_choice" = "mac" ]; then
-        brew install --cask alacritty
-      elif [ "$os_choice" = "linux" ]; then
-        sudo pacman -Syu alacritty
-      fi
-    else
-      echo -e "${GREEN}Alacritty is already installed.${NC}"
-    fi
-    echo -e "${YELLOW}Configuring Alacritty...${NC}"
-    mkdir -p ~/.config/alacritty
-    cp alacritty.toml ~/.config/alacritty/alacritty.toml
-    ;;
-  "wezterm")
-    if ! command -v wezterm &>/dev/null; then
-      if [ "$os_choice" = "mac" ]; then
-        brew install --cask wezterm
-      elif [ "$os_choice" = "linux" ]; then
-        sudo pacman -Syu wezterm
-      fi
-    else
-      echo -e "${GREEN}WezTerm is already installed.${NC}"
-    fi
-    echo -e "${YELLOW}Configuring WezTerm...${NC}"
-    mkdir -p ~/.config/wezterm
-    cp .wezterm.lua ~/.config/wezterm/wezterm.lua
-    ;;
-  "kitty")
-    if ! command -v kitty &>/dev/null; then
-      if [ "$os_choice" = "mac" ]; then
-        brew install --cask kitty
-      elif [ "$os_choice" = "linux" ]; then
-        sudo pacman -Syu kitty
-      fi
-    else
-      echo -e "${GREEN}Kitty is already installed.${NC}"
-    fi
-    echo -e "${YELLOW}Configuring Kitty...${NC}"
-    mkdir -p ~/.config/kitty
-    cp -r GentlemanKitty/* ~/.config/kitty
-    ;;
-  *)
-    echo -e "${YELLOW}No terminal emulator will be installed or configured.${NC}"
-    ;;
-  esac
-fi
-
-# Shared Steps (macOS, Linux, or WSL)
-
-# Step 3: Shell Configuration (Fish and Zsh)
+clone_repository_with_progress "https://github.com/angelcruz07/dotfiles.git" "AlexCode Dots" 20
+cd Gentleman.Dots || exit
