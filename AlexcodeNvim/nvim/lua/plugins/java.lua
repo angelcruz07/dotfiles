ea48c3e75e6e399a9b21d5b81ee26265d5b048ce
angelcruz07 <angelcruzsanchez126@gmail.com>
2025-01-25 17:41:51 -0600

    feat: add config for java

---
+++ AlexcodeNvim/nvim/lua/plugins/java.lua
@@ -0,0 +1,44 @@
+return {
+  "nvim-java/nvim-java",
+  config = false,
+  dependencies = {
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        servers = {
+          jdtls = {
+            settings = {
+              java = {
+                configuration = {
+                  runtimes = {
+                    {
+                      name = "JavaSE-23",
+                      path = "/usr/lib/jvm/java-23-openjdk/bin/java",
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        setup = {
+          jdtls = function()
+            -- Your nvim-java configuration goes here
+            require("java").setup({
+              -- root_markers = {
+              --   "settings.gradle",
+              --   "settings.gradle.kts",
+              --   "pom.xml",
+              --   "build.gradle",
+              --   "mvnw",
+              --   "gradlew",
+              --   "build.gradle",
+              --   "build.gradle.kts",
+              -- },
+            })
+          end,
+        },
+      },
+    },
+  },
+}
